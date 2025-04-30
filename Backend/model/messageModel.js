227a964982981483ead21217ec79a59d4a4ff5c1
nikeskhanal <154499524+nikeskhanal@users.noreply.github.com>
2025-04-30 20:38:56 +0545

    model created

---
+++ Backend/model/messageModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  messagepic: {
+    type: String, 
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
